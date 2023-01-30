42d21b7d41ca258b51034fce85921e6dd61a7d58
Alun King <alun.king1@southwales.ac.uk>
2023-01-30 11:04:23 +0000

    Initial Commit

---
+++ 5.1 KingTicTacToe/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  5.1 KingTicTacToe
+//
+//  Created by Alun King on 30/01/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 5.1 KingTicTacToe/__1_KingTicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  __1_KingTicTacToeApp.swift
+//  5.1 KingTicTacToe
+//
+//  Created by Alun King on 30/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct __1_KingTicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
